466b4eeb71656310d5e2db45d1ed1c3b1d4a06d5
tanughate05 <93458822+tanughate05@users.noreply.github.com>
2023-07-26 18:38:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+const sortedList = sortTitles(touristSpots)
+var ul = document.getElementById("list");
+ 
+sortedList.forEach(i => {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(i));
+	ul.appendChild(li);
+})
 const sortTitles(list) {
+	const data = []
+	
 	let newList = []
-	list.forEach(item => {
+	list.forEach((item, index) => {
 		let temp = item.split(" ").map(i => {
 			if(!(i.toLowerCase() === "the" || i.toLowerCase() === "an || i.toLowerCase() === "a)) {
 				return i;
 			}
 		})
-		newList.push(temp.join());
+		data.push({index: index, compareValue: temp.join(), actual:  item})
 	})
-	newList.sort((a ,b) => a<b ? 1 : (a>b ? -1 : 0))
-	return newList
+	newList.sort((a ,b) => a.compareValue<b.compareValue ? 1 : (a.compareValue>b.compareValue ? -1 : 0))
+	return newList.map(item => return item.actual)
 }
