4b839b9a00b72ce9e9685b6bfdd401a38b522c31
tanughate05 <93458822+tanughate05@users.noreply.github.com>
2023-07-26 18:24:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
-
+const sortTitles(list) {
+	let newList = []
+	list.forEach(item => {
+		let temp = item.split(" ").map(i => {
+			if(!(i.toLowerCase() === "the" || i.toLowerCase() === "an || i.toLowerCase() === "a)) {
+				return i;
+			}
+		})
+		newList.push(temp.join());
+	})
+	newList.sort((a ,b) => a<b ? 1 : (a>b ? -1 : 0))
+	return newList
+}
